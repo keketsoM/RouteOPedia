03d8298d51e1748546e9a3e4d4d32828134832e1
Keketso <kekekeketso03@gmail.com>
2024-11-13 16:31:37 +0200

    FirstRoute

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Headers from './Header';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import About from "./About";
+import Headers from "./Header";
+import Home from "./Home";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  
-   <div>
-    <Headers/>
-   </div>
-  
+  <BrowserRouter>
+    <Headers />
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/about" element={<About />}></Route>
+    </Routes>
+  </BrowserRouter>
 );
-
