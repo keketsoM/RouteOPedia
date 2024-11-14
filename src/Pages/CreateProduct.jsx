a54c4177b1b74673a26c799feea60ac5edf3c090
Keketso <kekekeketso03@gmail.com>
2024-11-14 14:38:10 +0200

    NavigateBack

--- src/Pages/CreateProduct.jsx
+++ src/Pages/CreateProduct.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
-
+import React from "react";
+import { useNavigate } from "react-router-dom";
 const CreateProduct = () => {
+  const navigate = useNavigate();
   return (
-    <div>CreateProduct</div>
-  )
-}
+    <div>
+      CreateProduct
+      <button
+        onClick={() => {
+          navigate(-1);
+        }}
+      >
+        Back
+      </button>
+    </div>
+  );
+};
 
-export default CreateProduct
+export default CreateProduct;
